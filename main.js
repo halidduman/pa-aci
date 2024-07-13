e073ddf0f84022b527b40040a491bb69fd6c9207
halid <halidduman60@gmail.com>
2024-07-13 19:28:16 +0300

    ufak-düzenlemeler

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function checkPassword() {
   var password = document.getElementById("password").value;
   // Burada şifre kontrolü yapabilirsiniz, örneğin:
   if (password === "paça") {
-    window.location.href = "index.html"; // Doğru şifre ise diğer sayfaya yönlendir
+    window.location.href = "site.html"; // Doğru şifre ise diğer sayfaya yönlendir
   } else {
     alert("Yanlış şifre! Tekrar deneyin.");
   }
